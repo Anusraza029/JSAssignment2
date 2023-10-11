6f3f6a28ccc809f2d30e7544298aefa205d41ac4
Anusraza09 <sanus8339@gmail.com>
2023-10-11 08:56:07 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var firstName ="My Name Is Muhammad Anus"
+var lastName ="Raza"
+var fullName = firstName + lastName
+ alert (fullName);
+
+var myAge ="Age 22"
+alert(myAge);
+
+var courseName ="Certified Mobile Application Development"
+alert(courseName)
+
+var pizza ="PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert (pizza)
+
+var email = 'Anusraza@gmail.com'
+alert("My Email Address is " + email +"!")
+  
+var book ="I am trying to learn from the Book A smarter way to learn JavaScript."
+alert (book)
+
+
+var textShow = "Yah! I can write HTML content through JavaScript"
+document.write("Yah! I can write HTML content through JavaScript")
+
+
+var end ="“ ▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ ”"
+alert (end);
